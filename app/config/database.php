9bffe818b17123daf6dd2356f04eb987ae985db5
ranggamauliza <ranggamauliza@gmail.com>
2014-04-05 02:27:06 +0700

    ignore

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'database',
+			'database'  => 'ditemukan',
 			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
