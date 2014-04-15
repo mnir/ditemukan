b8857bb285dc849b5079cfd0aa906466dbe25b9b
Fathur Rohman <fathur.rohman@fostrom.com>
2014-04-15 21:18:48 +0700

    routes

--- app/routes.php
+++ app/routes.php
@@ -3,6 +3,5 @@
 Route::get('/', array('uses' => 'HomeController@index'));
 Route::controller('users', 'UsersController');
 Route::controller('items', 'ItemsController');
-
 Route::controller('losts', 'LostsController');
 Route::controller('founds', 'FoundsController');
